53287d57d9d72ff3eaad8e2f684bb38e450fffe2
kimyeseul <kimyeseul9559@gmail.com>
2023-09-08 18:41:19 +0900

    first_commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import my_package.math_operations as mo
+#from my_package import math_operations as mo #위와 같은 의미
+#from my_package.math_operations import add, sub, mul, div #mo를 떼고 쓰는 방법
+
+result = mo.add(3, 5)
+print(result)
+
+result2 = mo.mul(3, 5)
+print(result2)
